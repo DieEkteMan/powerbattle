d917dbd9503503f72045da4d4d85082edf6664cb
r0wi3 <ro.mole68@gmail.com>
2016-12-09 15:23:05 +0100

    Update

--- gamemode/cl_init.lua
+++ gamemode/cl_init.lua
@@ -1,8 +1,7 @@
 include( "shared.lua" )
 
-
 // Derma menu to choose team
-function set_team()
+net.Receive("menu", function()
  
 local frame = vgui.Create( "DFrame" )
 frame:SetPos(500, 500) 
@@ -16,87 +15,21 @@ frame:MakePopup()
 team_1 = vgui.Create( "DButton", frame ) // Team 1 = 2 || Team 2 = 3
 team_1:SetPos( frame:GetTall() / 2, 5 )
 team_1:SetSize( 50, 100 )
-team_1:SetText( "Team 1" )
+team_1:SetText( "Player Team" )
 team_1.DoClick = function() 
-    RunConsoleCommand( "team_2" )
+    net.Start("player")
+    net.SendToServer()
     frame:Close()
 end
  
 team_2 = vgui.Create( "DButton", frame )
 team_2:SetPos( frame:GetTall() / 2, 105 ) 
 team_2:SetSize( 50, 100 )
-team_2:SetText( "Team 2" )
+team_2:SetText( "Spectate" )
 team_2.DoClick = function()
-    RunConsoleCommand( "team_3" )
+    net.Start("spectator")
+    net.SendToServer()
     frame:Close()
 end
 
-end
-
-function spec_menu()
- 
-local frame = vgui.Create( "DFrame" )
-frame:SetPos(500, 500) 
-frame:SetSize( 500, 385 )
-frame:SetTitle( "Change Team" )
-frame:SetVisible( true )
-frame:SetDraggable( false )
-frame:ShowCloseButton( false )
-frame:MakePopup()
- 
-team_1 = vgui.Create( "DButton", frame ) // Team 1 = 2 || Team 2 = 3
-team_1:SetPos( frame:GetTall() / 2, 5 )
-team_1:SetDisabled( true )
-team_1:SetSize( 50, 100 )
-team_1:SetText( "Team 1" )
-team_1.DoClick = function() 
-    RunConsoleCommand( "team_2" )
-    frame:Close()
-end
- 
-team_2 = vgui.Create( "DButton", frame )
-team_2:SetPos( frame:GetTall() / 2, 105 ) 
-//team_2:SetDisabled( true )
-team_2:SetSize( 50, 100 )
-team_2:SetText( "Team 2" )
-team_2.DoClick = function()
-    RunConsoleCommand( "team_3" )
-    frame:Close()
-end
-
-end
-
-function playermenu()
- 
-local frame = vgui.Create( "DFrame" )
-frame:SetPos(500, 500) 
-frame:SetSize( 500, 385 )
-frame:SetTitle( "Change Team" )
-frame:SetVisible( true )
-frame:SetDraggable( false )
-frame:ShowCloseButton( false )
-frame:MakePopup()
- 
-team_1 = vgui.Create( "DButton", frame ) // Team 1 = 2 || Team 2 = 3
-team_1:SetPos( frame:GetTall() / 2, 5 )
-team_1:SetSize( 50, 100 )
-team_1:SetText( "Team 1" )
-team_1.DoClick = function() 
-    RunConsoleCommand( "team_2" )
-    frame:Close()
-end
- 
-team_2 = vgui.Create( "DButton", frame )
-team_2:SetPos( frame:GetTall() / 2, 105 )
-team_2:SetDisabled( true ) 
-team_2:SetSize( 50, 100 )
-team_2:SetText( "Team 2" )
-team_2.DoClick = function()
-    RunConsoleCommand( "team_3" )
-    frame:Close()
-end
- 
-end
-concommand.Add( "team_menu", set_team )
-concommand.Add( "spec_menu", spec_menu )
-concommand.Add( "playermenu", playermenu)  // This can be done in a different way but this will be done later...
+end)
