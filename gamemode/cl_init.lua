0a74698ff2e7b06449fe2c9bf08250401283b42d
Rowen Molenaar <ro.mole68@gmail.com>
2016-12-09 17:53:53 +0100

    Big update

--- gamemode/cl_init.lua
+++ gamemode/cl_init.lua
@@ -1,35 +1,22 @@
 include( "shared.lua" )
+include( "vgui/main_menu.lua")
 
 // Derma menu to choose team
-net.Receive("menu", function()
- 
-local frame = vgui.Create( "DFrame" )
-frame:SetPos(500, 500) 
-frame:SetSize( 500, 385 )
-frame:SetTitle( "Change Team" )
-frame:SetVisible( true )
-frame:SetDraggable( false )
-frame:ShowCloseButton( false )
-frame:MakePopup()
- 
-team_1 = vgui.Create( "DButton", frame ) // Team 1 = 2 || Team 2 = 3
-team_1:SetPos( frame:GetTall() / 2, 5 )
-team_1:SetSize( 50, 100 )
-team_1:SetText( "Player Team" )
-team_1.DoClick = function() 
-    net.Start("player")
-    net.SendToServer()
-    frame:Close()
-end
- 
-team_2 = vgui.Create( "DButton", frame )
-team_2:SetPos( frame:GetTall() / 2, 105 ) 
-team_2:SetSize( 50, 100 )
-team_2:SetText( "Spectate" )
-team_2.DoClick = function()
-    net.Start("spectator")
-    net.SendToServer()
-    frame:Close()
-end
+net.Receive("f2menu", function()
+	if( !MainMenu ) then
+		MainMenu = vgui.Create( "main_menu")
+		MainMenu:SetVisible(false)
+	end
 
+	if( MainMenu:IsVisible() ) then
+		MainMenu:SetVisible( false )
+	else
+		MainMenu:SetVisible( true )
+	end
+	
 end)
+
+
+
+	//net.Start("spectator")
+    //net.SendToServer()
