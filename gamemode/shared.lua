15a8c01961321814a20dd19bc3d0175750f2485e
r0wi3 <ro.mole68@gmail.com>
2016-12-09 13:40:45 +0100

    Update

--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -3,6 +3,23 @@ GM.Author = "DieEkteMan & Rowie"
 GM.Email = "dedennisis@gmail.com & ro.mole68@gmail.com"
 GM.Website = "N/A"
 
+
+// Here we will add the teams we've got
+team.SetUp( 1, "Player", Color( 125, 125, 125, 255 ) )
+team.SetUp( 2, "Spectator", Color( 125, 125, 125, 255 ) )
+
 function GM:Initialize()
-	
+
+end
+
+function GM:PlayerInitialSpawn(ply)
+		ply:SetTeam( 1 )
+		ply:ChatPrint("Welcome to our Power Battle Server! You've been put into the player team.")
+end
+
+function GM:PlayerLoadout(ply)
+
+	if ply:Team() == 1 then 
+		ply:Give( "weapon_physcannon" )
+	end
 end
