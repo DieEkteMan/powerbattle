6ee5d028ce7084ad17c28c54dfb0fa8eeadd593e
Rowen Molenaar <ro.mole68@gmail.com>
2016-12-11 17:41:03 +0100

    Updated Scoreboard
    Added some new things to the scoreboard

--- gamemode/vgui/scoreboard.lua
+++ gamemode/vgui/scoreboard.lua
@@ -1,12 +1,12 @@
 local ScoreboardDerma = nil
 local PlayerList = nil
 
-function GM:ScoreboardShow()
+function GM:ScoreboardShow(ply)
 	if !IsValid(ScoreboardDerma) then
 		ScoreboardDerma = vgui.Create('DFrame')
 		ScoreboardDerma:SetSize(900,700)
 		ScoreboardDerma:SetPos(ScrW() / 2 -450, ScrH() / 2 - 350)
-		ScoreboardDerma:SetTitle('Scoreboard')
+		ScoreboardDerma:SetTitle('PowerBattle Gaming')
 		ScoreboardDerma:SetDraggable(false)
 		ScoreboardDerma:ShowCloseButton(false)
 		ScoreboardDerma.Paint = function()
@@ -32,10 +32,23 @@ function GM:ScoreboardShow()
 			PlayerPanel.Paint = function()
 				draw.RoundedBox(0, 0, 0, PlayerPanel:GetWide(), PlayerPanel:GetTall(), Color(50, 50, 50, 255))
 				draw.RoundedBox(0, 0, 49, PlayerPanel:GetWide(), 1, Color(255, 255, 255, 255))
+
+				draw.SimpleText(v:GetName(),'Font1',20,15, Color(255,255,255))
+				draw.SimpleText("Rank: "..v:GetUserGroup(), "Font2", 200, 15, Color( 255, 255, 255 ), TEXT_ALIGN_CENTER)
+				draw.SimpleText("Kills: "..v:Frags(), "Font3", PlayerList:GetWide() - 120, 15, Color( 255, 255, 255 ), TEXT_ALIGN_RIGHT)
+				draw.SimpleText("Deaths: "..v:Deaths(), "Font3", PlayerList:GetWide() - 120, 30, Color( 255, 255, 255 ), TEXT_ALIGN_RIGHT)
+				draw.SimpleText("Ping "..v:Ping(), "Font3", PlayerList:GetWide() - 15, 15, Color( 255, 255, 255 ), TEXT_ALIGN_RIGHT)
+				if v:GetUserGroup() == "superadmin" then
+					surface.SetMaterial( Material("icon16/shield.png") )
+					surface.DrawTexturedRect( PlayerList:GetWide() - 95, 15, 16, 16 )
+				elseif v:GetUserGroup() != "superadmin" then
+					surface.SetMaterial( Material("icon16/user.png") )
+					surface.DrawTexturedRect( PlayerList:GetWide() - 95, 15, 16, 16 )
+				end
 				
-				draw.SimpleText(v:GetName(),'DermaDefault',20,10, Color(255,255,255))
+				end
 			end
-		end
+
 	
 		ScoreboardDerma:Show()
 		ScoreboardDerma:MakePopup()
@@ -48,3 +61,57 @@ function GM:ScoreboardHide()
 		ScoreboardDerma:Hide()
 	end
 end
+
+surface.CreateFont( "Font1", {
+	font = "Arial", -- Use the font-name which is shown to you by your operating system Font Viewer, not the file name
+	extended = false,
+	size = 19,
+	weight = 500,
+	blursize = 0,
+	scanlines = 0,
+	antialias = true,
+	underline = false,
+	italic = false,
+	strikeout = false,
+	symbol = false,
+	rotary = false,
+	shadow = false,
+	additive = false,
+	outline = false,
+} )
+
+surface.CreateFont( "Font2", {
+	font = "Arial", -- Use the font-name which is shown to you by your operating system Font Viewer, not the file name
+	extended = false,
+	size = 17,
+	weight = 500,
+	blursize = 0,
+	scanlines = 0,
+	antialias = true,
+	underline = false,
+	italic = false,
+	strikeout = false,
+	symbol = false,
+	rotary = false,
+	shadow = false,
+	additive = false,
+	outline = false,
+} )
+
+surface.CreateFont( "Font3", {
+	font = "Arial", -- Use the font-name which is shown to you by your operating system Font Viewer, not the file name
+	extended = false,
+	size = 15,
+	weight = 500,
+	blursize = 0,
+	scanlines = 0,
+	antialias = true,
+	underline = false,
+	italic = false,
+	strikeout = false,
+	symbol = false,
+	rotary = false,
+	shadow = false,
+	additive = false,
+	outline = false,
+} )
